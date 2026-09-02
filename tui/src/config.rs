19ef5ba772f1ca8255c7689b4e342a0e7966398e
Amr Morcy <3mr5aled.dev@gmail.com>
2026-09-02 22:26:58 +0300

    fix(config): strictly fail when custom config path does not exist

--- tui/src/config.rs
+++ tui/src/config.rs
@@ -128,12 +128,10 @@ impl DevConfig {
 
     pub fn load_from(custom_path: &Option<PathBuf>) -> Result<Self> {
         if let Some(p) = custom_path {
-            if p.is_file() {
-                let content = std::fs::read_to_string(p)
-                    .with_context(|| format!("Cannot read config from {}", p.display()))?;
-                return serde_json::from_str(&content)
-                    .with_context(|| format!("Failed to parse config file {}", p.display()));
-            }
+            let content = std::fs::read_to_string(p)
+                .with_context(|| format!("Cannot read config from {}", p.display()))?;
+            return serde_json::from_str(&content)
+                .with_context(|| format!("Failed to parse config file {}", p.display()));
         }
         Self::load()
     }
